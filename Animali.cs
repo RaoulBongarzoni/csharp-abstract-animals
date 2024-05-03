8427e09cb6e3153af289c1bc74adac2c92d87339
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-05-03 17:48:09 +0200

    Playing around with Interfaces and Abstract Classes

---
+++ Animali.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+
+    //calsse astratta del primo esercizio
+    public abstract class Animale
+    {
+
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+
+        public abstract void Mangia();
+
+
+        public abstract void Verso();
+
+
+    }
+
+
+    //interfacce del secondo esercizo
+
+    public interface IVolante
+    {
+        public void Vola();
+    }
+
+    public interface INuotante
+    {
+        public void Nuota();
+    }
+    //************************
+
+
+    //classi degli animali *****
+    public class Cane : Animale
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Carne");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Bau Bau");
+        }
+
+    }
+    public class Delfino : Animale, INuotante //interfaccia inserita in corrispondenza del secondo esercizio
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Pesce");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("*Ultrasuoni*");
+        }
+        
+        public void Nuota()//INuotante
+        {
+            Console.WriteLine("Sto Nuotandooo!!");
+        }
+
+
+
+    }
+    public class Aquila : Animale, IVolante //interfaccia inserita in corrispondenza del secondo esercizio
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Piccoli roditori");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("*Stride*");
+        }
+
+        public void Vola() //IVolante 
+        {
+            Console.WriteLine("Sto Volandoo!!");
+        }
+
+    }
+    public class Passerotto : Animale, IVolante //interfaccia inserita in corrispondenza del secondo esercizio
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Semi e bacche");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("*Cirp Cirp*");
+        }
+        public void Vola()//IVolante
+        {
+            Console.WriteLine("Sto Volandoo!!");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,113 @@
-﻿namespace csharp_abstract_animals
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.RegularExpressions;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_abstract_animals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-      
+
+
+
+
+            /**Esercizio 1 : classi astratte**
+            Dobbiamo realizzare un programma che rappresenti il regno animale.
+            Creiamo quindi una classe astratta Animale e delle classi che la estendono : Cane, Passerotto, Aquila, Delfino
+            Vogliamo che gli animali abbiano i seguenti metodi
+            - void Dormi()(mostra a video “Zzz”)
+            - void Verso()(mostra a video il verso fatto dall'animale specifico)
+            - void Mangia()(mostra a video quello che mangia: erba, carne, ...?)
+            Quali di questi metodi devono essere implementati nella classe astratta e quali devono essere lasciati astratti e implementati dalle classi che la estendono ?
+            Una volta create le classi, nel programma istanziare alcuni animali e utilizzare i metodi implementati.*/
+
+            Console.WriteLine("istanzio qualche animale");
+
+
+            List<Animale> animali = new List<Animale>{
+                new Aquila(),
+                new Cane(),
+                new Delfino(),
+                new Passerotto()
+
+
+            };
+
+
+            foreach (Animale animale in animali)
+            {
+                Console.WriteLine($"{animale.GetType().Name}");
+                Console.WriteLine("\nil verso che fa è:\n ");
+                animale.Verso();
+                Console.WriteLine("\ne mangia:\n");
+                animale.Mangia();
+                Console.WriteLine("\n***************************** \n \n");
+            }
+
+
+            /*
+               **Esercizio 2 : interfacce**
+               Alcuni degli animali che abbiamo creato volano, altri nuotano.
+               Gli animali che volano hanno il seguente metodo :
+               - void Vola() (mostra a video “Sto volando!!!”)
+               Gli animali che nuotano hanno il seguente metodo :
+               - void Nuota() (mostra a video “Sto nuotando!!!”)
+               Scrivere un programma avente 2 metodi :
+               - void FaiVolare(IVolante animale)
+               - void FaiNuotare(INuotante animale)
+               Questi metodi prendono come parametro un animale che può o volare / nuotare e richiamano il corrispondente metodo Vola() / Nuota().
+               Scrivere un programma che istanzi animali che volano o nuotano e richiamare i metodi FaiVolare / FaiNuotare passandoglieli come parametro.*/
+
+            Console.WriteLine("istanzio qu" +
+                "alche animale");
+            List<Animale> animali2 = new List<Animale>{
+                new Aquila(),
+                new Cane(),
+                new Delfino(),
+                new Passerotto(),
+                new Passerotto()
+
+            };
+
+
+
+
+            foreach (Animale animale in animali2)
+            {
+                Console.WriteLine($"\n{animale.GetType().Name} dice :" );
+                if (animale is IVolante) //controllo l'interfaccia
+                {
+                    FaiVolare((IVolante)animale);
+                }else if (animale is INuotante)
+                {
+                    FaiNuotare((INuotante)animale);
+                }else
+                {
+                    Console.WriteLine("eh io non volo ne nuoto...Mi metterò a dormire..");
+                    animale.Dormi();
+                }
+
+                
+
+            }
+
+
+
+
+            //funzioni per far volare e per far nuotare
+            void FaiNuotare(INuotante elem)
+            {
+                elem.Nuota();
+
+            }
+
+            void FaiVolare(IVolante elem)
+            {
+                elem.Vola();
+
+            }
         }
     }
 }
